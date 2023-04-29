d5f1a85f93ffb8241692f0bae2122c548954fe0e
Rikarnto Bariampa <r.bariampa@space-innovation.org>
2023-04-29 11:18:27 +0300

    Added ExecutionEngine exception.

---
+++ src/ExecutionEngineException.cpp
@@ -0,0 +1,37 @@
+/** \if false
+ * MIT License
+ * 
+ * Copyright (c) 2023 SPIN - Space Innovation
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * \endif
+ */
+
+#include "SPIN/ExecutionEngineException.hpp"
+
+#include <string>
+
+#include "SPIN/ExceptionType.hpp"
+
+SPIN::ExecutionEngineException::ExecutionEngineException(std::string message)
+{
+    this->_exceptionType = SPIN::ExceptionType::ExecutionEngineException;
+    this->_exceptionTypeString = "System.ExecutionEngineException";
+    this->_message = message;
+}
