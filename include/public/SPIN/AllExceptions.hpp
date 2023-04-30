f0a69044612050dad5a81159b4eca45a2007905f
Rikarnto Bariampa <r.bariampa@space-innovation.org>
2023-04-30 11:16:46 +0300

    Add include all exceptions header file.

---
+++ include/public/SPIN/AllExceptions.hpp
@@ -0,0 +1,82 @@
+/** \if false
+ * MIT License
+ * 
+ * Copyright (c) 2023 SPIN - Space Innovation
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * \endif
+ */
+
+#pragma once
+
+#include <SPIN/ExceptionType.hpp>
+
+#include <SPIN/Exception.hpp>
+
+#include <SPIN/SystemException.hpp>
+#include <SPIN/AccessViolationException.hpp>
+#include <SPIN/AggregateException.hpp>
+#include <SPIN/ApplicationException.hpp>
+#include <SPIN/ArgumentException.hpp>
+#include <SPIN/ArgumentNullException.hpp>
+#include <SPIN/ArgumentOutOfRangeException.hpp>
+#include <SPIN/ArithmeticException.hpp>
+#include <SPIN/ContextMarshalException.hpp>
+#include <SPIN/DataMisalignedException.hpp>
+#include <SPIN/DivideByZeroException.hpp>
+#include <SPIN/DllNotFoundException.hpp>
+#include <SPIN/ExecutionEngineException.hpp>
+#include <SPIN/FieldAccessException.hpp>
+#include <SPIN/FormatException.hpp>
+#include <SPIN/IndexOutOfRangeException.hpp>
+#include <SPIN/InsufficientMemoryException.hpp>
+#include <SPIN/InvalidCastException.hpp>
+#include <SPIN/InvalidOperationException.hpp>
+#include <SPIN/InvalidTimeZoneException.hpp>
+#include <SPIN/MemberAccessException.hpp>
+#include <SPIN/MethodAccessException.hpp>
+#include <SPIN/MissingFieldException.hpp>
+#include <SPIN/MissingMemberException.hpp>
+#include <SPIN/MissingMethodException.hpp>
+#include <SPIN/NotFiniteNumberException.hpp>
+#include <SPIN/NotImplementedException.hpp>
+#include <SPIN/NotSupportedException.hpp>
+#include <SPIN/NullReferenceException.hpp>
+#include <SPIN/ObjectDisposedException.hpp>
+#include <SPIN/OperationCanceledException.hpp>
+#include <SPIN/OutOfMemoryException.hpp>
+#include <SPIN/OverflowException.hpp>
+#include <SPIN/PlatformNotSupportedException.hpp>
+#include <SPIN/StackOverflowException.hpp>
+#include <SPIN/TimeoutException.hpp>
+#include <SPIN/TimeZoneNotFoundException.hpp>
+#include <SPIN/TypeAccessException.hpp>
+#include <SPIN/TypeInitializationException.hpp>
+#include <SPIN/UnauthorizedAccessException.hpp>
+#include <SPIN/UriFormatException.hpp>
+
+#include <SPIN/IOException.hpp>
+#include <SPIN/DirectoryNotFoundException.hpp>
+#include <SPIN/DriveNotFoundException.hpp>
+#include <SPIN/EndOfStreamException.hpp>
+#include <SPIN/FileFormatException.hpp>
+#include <SPIN/FileNotFoundException.hpp>
+#include <SPIN/InternalBufferOverflowException.hpp>
+#include <SPIN/InvalidDataException.hpp>
+#include <SPIN/PathTooLongException.hpp>
